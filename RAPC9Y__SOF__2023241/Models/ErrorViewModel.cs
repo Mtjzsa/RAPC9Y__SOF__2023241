d27f5dc0327121d583a136a172ddd0045ea84106
Mtjzsa <mate.jozsa03@gmail.com>
2024-01-05 14:56:43 +0100

    Add project files.

---
+++ RAPC9Y__SOF__2023241/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RAPC9Y__SOF__2023241.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
